dbde64c028a66db3c058458f7079e61db93ae5bf
James Tuck <JamesjTuck@gmail.com>
2020-10-17 13:30:18 -0600

    removed console logs from script file and got local storage saves working

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 $(document).ready(function () {
 
     // variables for dates
-    var today = moment().format("DD MMM YYYY");
     var day = moment().format("DD");
     var month = moment().format("MMMM");
     var year = moment().format("YYYY");
@@ -12,14 +11,6 @@ $(document).ready(function () {
 
     // variable for saved content
     var savedContent = JSON.parse(localStorage.getItem("savedContent")) || [];
-
-    // console log to figure out date formatting
-    console.log(today);
-    console.log(day);
-    console.log(month);
-    console.log(year);
-    console.log(hour);
-    console.log(todayComposite);
 
     // initial function adds today to the DOM and calls the addTimeBlocks function
     function init(){
@@ -68,18 +59,18 @@ $(document).ready(function () {
                 id: 17
             }
         ];
-
+        // loop through array of times to populate the dom
         $.each(times, function (i, item) {
-
+            // variables to create DOM elements
             var divRow = $("<div>").attr("class", "row").attr("id", item.id);
             var divTimeCol = $("<div>").attr("class", "col-sm-2 hour text-center").text(item.value);
             var textareaCol = $("<textarea>").attr("class", "col-sm-8 time-block").attr("id", item.id + "TextArea");
             var btnSaveCol = $("<button>").attr("class", "col-sm-2 saveBtn").attr("id",item.id + "SaveBtn").html("<i class=\"far fa-save\"></i>");
-
+            // append the elements to the div row
             divRow.append(divTimeCol);
             divRow.append(textareaCol);
             divRow.append(btnSaveCol);
-
+            // update classes based on time
             if(item.id < hour){
                 divRow.attr("class", "row time-block past");
             } else if (item.id > hour){
@@ -87,8 +78,9 @@ $(document).ready(function () {
             } else {
                 divRow.attr("class", "row time-block present");
             };
-
+            // append previous elements to the DOM
             timeDiv.append(divRow);
+            // populate any saved content
             $.each(savedContent,function(index, value){
                 $("#" + value.time + "TextArea").text(value.text);
             });
@@ -98,30 +90,20 @@ $(document).ready(function () {
     
     // click event for save buttons - saves user inputs
     $(document).on("click", ".saveBtn", function (event) {
+        // set id to the clicked elements id attribute
         var id = $(this).attr("id");
+        // remove SaveBtn from the id so I am left with only a number
         id = id.replace("SaveBtn","");
-        console.log(id);
+        // set input to the text data that corresponds to the clicked save button
         var input = $("#" + id + "TextArea").val();
-        console.log(input);
+        // add data to be saved to the array
         savedContent.push({
             time: id,
             text: input
         })
+        // save the users input to local storage
         localStorage.setItem("savedContent", JSON.stringify(savedContent));
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     // initial function to start the whole process 
     init();
